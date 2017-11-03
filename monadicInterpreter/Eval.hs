d32253a01ff83caf5a6279a14ba9453ed19c2b63
dejawu <juihsuan.wu@ens.fr>
2017-11-03 23:26:37 +0100

    Fix some small mistakes

--- monadicInterpreter/Eval.hs
+++ monadicInterpreter/Eval.hs
@@ -3,13 +3,13 @@ module Eval where
 import Ast
 import Env
 
-eval1 :: Exp -> Dict -> M Int
-eval1 exp dict = case exp of
+eval :: Exp -> Dict -> M Integer
+eval exp dict = case exp of
     Const n -> return n
     Var x -> getfrom $ position x dict
     Binop op exp1 exp2 -> do
-        v1 <- eval1 exp1 dict;
-        v2 <- eval1 exp2 dict;
+        v1 <- eval exp1 dict;
+        v2 <- eval exp2 dict;
         return $ case op of
             Plus    -> v1 + v2
             Minus   -> v1 - v2
@@ -22,31 +22,31 @@ eval1 exp dict = case exp of
             Equal   -> if v1 == v2 then 1 else 0
             Unequal -> if v1 /= v2 then 1 else 0
 
-interpret1 :: Com -> Dict -> M ()
-interpret1 stmt dict = case stmt of
+interpret :: Com -> Dict -> M ()
+interpret stmt dict = case stmt of
     Assign name exp -> do 
-        v <- eval1 exp dict
-        write (position dict name) v
+        v <- eval exp dict
+        write (position name dict) v
     Seq stmts -> do
-        mapM_ (\stmt -> interpret1 stmt dict) stmts
+        mapM_ (\stmt -> interpret stmt dict) stmts
         return ()
     Cond exp stmt1 stmt2 -> do
-        value <- eval1 exp dict
+        value <- eval exp dict
         if value == 1
-           then interpret1 stmt1 dict
-           else interpret1 stmt2 dict
+           then interpret stmt1 dict
+           else interpret stmt2 dict
     While exp stmt' -> 
         let loop () = do 
-            value <- eval1 exp dict
+            value <- eval exp dict
             if value == 0 
                then return ()
-               else do {interpret1 stmt' dict; loop ()}
+               else do {interpret stmt' dict; loop ()}
         in loop ()
     Let name exp stmt' -> do
-        value <- eval1 exp dict
+        value <- eval exp dict
         push value
-        interpret1 stmt' exp (name:dict)
-        pop value
-    Print exp ->
-        value <- eval1 exp dict
+        interpret stmt' (name:dict)
+        pop
+    Print exp -> do
+        value <- eval exp dict
         output value
